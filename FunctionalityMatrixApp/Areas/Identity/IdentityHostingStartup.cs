02163655a49d317eb016bfb5e4eb88a88fb2cc25
mjack31 <shark3113@gmail.com>
2019-04-21 00:51:48 +0200

    Add project files.

---
+++ FunctionalityMatrixApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using FunctionalityMatrixApp.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(FunctionalityMatrixApp.Areas.Identity.IdentityHostingStartup))]
+namespace FunctionalityMatrixApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
